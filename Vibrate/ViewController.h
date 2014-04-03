5417e0ed06ab310f1f1fdae9e98e745f62bca3d5
devalios <partialbert@gmail.com>
2014-04-03 17:30:54 +0200

    Release

--- Vibrate/ViewController.h
+++ Vibrate/ViewController.h
@@ -5,7 +5,7 @@
 //  Created by Parti Albert on 2012.12.17..
 //  Copyright (c) 2012 Parti Albert. All rights reserved.
 //
-
+//
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
